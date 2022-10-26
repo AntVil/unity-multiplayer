41aeaba022630e4b2788782b85dfb68cece912f3
Anton Böhler <antboehler@gmail.com>
2022-10-26 15:32:12 +0200

    add model teleportation areas

--- Assets/Scripts/NetworkMap/NetworkMap.cs
+++ Assets/Scripts/NetworkMap/NetworkMap.cs
@@ -26,6 +26,10 @@ public class NetworkMap : Unity.Netcode.NetworkBehaviour
     private bool[] updateAvailableModelImages;
     private bool[] updateAvailableModels;
 
+    public GameObject teleportationArea;
+    public float teleportationAreaAccuracy = 0.25f;
+    public float teleportationAreaSlopeLimit = 0.01f;
+
     public void Start(){
         client = new WebClient();
 
@@ -162,20 +166,124 @@ public class NetworkMap : Unity.Netcode.NetworkBehaviour
     }
 
     private void UpdateModel(int modelId){
+        updateAvailableModels[modelId] = false;
+
         GameObject loadedObject = new OBJLoader().Load(modelPath + $"model_{modelId}.obj");
         loadedObject.transform.Rotate(-90, 0, 0);
-
-        // add collision
-        foreach (Transform child in loadedObject.transform){
-            child.gameObject.AddComponent<MeshCollider>();
-        }
 
         // add backfaces to meshes (adds collision & better rendering at performance cost)
         foreach(MeshFilter meshFilter in loadedObject.GetComponentsInChildren<MeshFilter>()){
             meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0).Concat(meshFilter.mesh.GetIndices(0).Reverse()).ToArray(), MeshTopology.Triangles, 0);
         }
 
-        updateAvailableModels[modelId] = false;
+        // add collision & teleportation
+        foreach (Transform child in loadedObject.transform){
+            MeshCollider collider = child.gameObject.AddComponent<MeshCollider>();
+
+            Bounds bounds = new Bounds(child.position, Vector3.zero);
+            bounds.Encapsulate(child.GetComponent<Renderer>().bounds);
+
+            float[] xArray = new float[(int)Math.Floor(bounds.size.x / teleportationAreaAccuracy)];
+            float[] zArray = new float[(int)Math.Floor(bounds.size.z / teleportationAreaAccuracy)];
+            for(int i=0;i<xArray.Length;i++){
+                xArray[i] = bounds.min.x + teleportationAreaAccuracy * i;
+            }
+            for(int i=0;i<zArray.Length;i++){
+                zArray[i] = bounds.min.z + teleportationAreaAccuracy * i;
+            }
+
+            float[,] topView = new float[xArray.Length, zArray.Length];
+            for(int i=0;i<xArray.Length;i++){
+                for(int j=0;j<zArray.Length;j++){
+                    RaycastHit hit;
+                    if(collider.Raycast(new Ray(new Vector3(xArray[i], bounds.max.y + 1, zArray[j]), Vector3.down), out hit, 100)){
+                        topView[i, j] = hit.point.y;
+                    }
+                }
+            }
+
+            // remove lonely points
+            for(int i=0;i<xArray.Length;i++){
+                for(int j=0;j<zArray.Length;j++){
+                    if(topView[i, j] != 0.0f){
+                        int neighbours = 0;
+                        
+                        if(i != 0 && Math.Abs(topView[i-1, j] - topView[i, j]) < teleportationAreaSlopeLimit) neighbours++;
+                        if(j != 0 && Math.Abs(topView[i, j-1] - topView[i, j]) < teleportationAreaSlopeLimit) neighbours++;
+                        if(i != xArray.Length-1 && Math.Abs(topView[i+1, j] - topView[i, j]) < teleportationAreaSlopeLimit) neighbours++;
+                        if(j != zArray.Length-1 && Math.Abs(topView[i, j+1] - topView[i, j]) < teleportationAreaSlopeLimit) neighbours++;
+
+                        if(neighbours == 0) topView[i, j] = 0.0f;
+                    }
+                }
+            }
+
+            List<float> unique = new List<float>();
+            
+            for(int i=0;i<xArray.Length;i++){
+                for(int j=0;j<zArray.Length;j++){
+                    bool isNew = true;
+                    for(int k=0;k<unique.Count;k++){
+                        if(Math.Abs(unique[k] - topView[i, j]) < teleportationAreaSlopeLimit){
+                            isNew = false;
+                            break;
+                        }
+                    }
+                    if(isNew){
+                        unique.Add(topView[i, j]);
+                    }
+                }
+            }
+
+            foreach(float height in unique){
+                bool[,] layer = new bool[xArray.Length, zArray.Length];
+
+                for(int i=0;i<xArray.Length;i++){
+                    for(int j=0;j<zArray.Length;j++){
+                        layer[i, j] = Math.Abs(topView[i, j] - height) < teleportationAreaSlopeLimit;
+                    }
+                }
+
+                for(int i=0;i<xArray.Length;i++){
+                    for(int j=0;j<zArray.Length;j++){
+                        if(layer[i, j]){
+                            int size = 1;
+                            bool extendable = true;
+                            while(extendable){
+                                if(i + size > xArray.Length-1 || j + size > zArray.Length-1){
+                                    break;
+                                }
+                                
+                                for(int k=0;k<size;k++){
+                                    if(i + k > xArray.Length-1 || j + k > zArray.Length-1 || !layer[i + k, j + size] || !layer[i + size, j + k]){
+                                        extendable = false;
+                                        break;
+                                    }
+                                }
+
+                                if(extendable){
+                                    size++;
+                                }
+                            }
+
+                            //print(size);
+
+                            for(int p=0;p<size;p++){
+                                for(int q=0;q<size;q++){
+                                    layer[i+p, j+q] = false;
+                                }
+                            }
+
+                            float translation = (size / 2) * teleportationAreaAccuracy;
+                            float scale = 0.1f * size * teleportationAreaAccuracy;
+                            GameObject square = Instantiate(teleportationArea, new Vector3(xArray[i] + translation, height + 0.01f, zArray[j] + translation), Quaternion.identity);
+                            square.transform.localScale = new Vector3(scale, scale, scale);
+                            square.transform.parent = child;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public void UpdateAllModelFiles(){
@@ -183,6 +291,8 @@ public class NetworkMap : Unity.Netcode.NetworkBehaviour
             UpdateModelNameFile(i);
             UpdateModelImageFile(i);
             UpdateModelFile(i);
+            
+            break;
         }
     }
 
